c370c6c7990168bc6ffaa96020625845c93a9534
Mateusz <mateusz.skrzypczyk1@hotmail.com>
2021-03-08 00:41:12 +0100

    [COT-3] Generate-backend-project

---
+++ backend/src/main/java/com/codetris/CodetrisApplication.java
@@ -0,0 +1,13 @@
+package com.codetris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodetrisApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/codetris/CodetrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodetrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
