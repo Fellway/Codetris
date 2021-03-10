093f3f2545e8f7c1a7bacc0ab655e84f167b795c
Mateusz <mateusz.skrzypczyk1@hotmail.com>
2021-03-10 21:56:25 +0100

    [COT-11] Added main menu component

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/components/*/*/*.stories.js'],
+  stories: ['../src/components/**/**/*.stories.js'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
