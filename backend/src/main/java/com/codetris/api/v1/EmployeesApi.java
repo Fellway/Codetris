3a73cc6f24c54eb805d8f27e1413ac3cedc05eb8
MacMarMor <60295795+MacMarMor@users.noreply.github.com>
2021-03-11 22:05:49 +0100

    [COT-8] Added Emoloyees endpoint

--- backend/src/main/java/com/codetris/api/v1/EmployeesApi.java
+++ backend/src/main/java/com/codetris/api/v1/EmployeesApi.java
@@ -1,44 +1,16 @@
 package com.codetris.api.v1;
 
 import com.codetris.dto.response.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/foos")
-
+@RequestMapping("/api/v1/employees")
 public class EmployeesApi {
-    @RequestBody Employee employee;
-
-    @GetMapping
-    public List<Employee> findAll() {
-        return ("Hello Word");
-    }
-
-    @GetMapping(value = "/{id}")
-    public Employee findById(@PathVariable("id") Long id) {
-        return ("Hello Word");
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Long create(@RequestBody Employee employee) {
-        return ("Hello Word");
+    public void create(@RequestBody final Employee employee) {
+        System.out.println("Hello Word");
     }
-
-    @PutMapping(value = "/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void update(@PathVariable( "id" ) Long id, @RequestBody Employee employee) {
-        return ("Hello Word");
-    }
-
-    @DeleteMapping(value = "/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void delete(@PathVariable("id") Long id) {
-        return ("Hello Word");
-    }
-
 }
