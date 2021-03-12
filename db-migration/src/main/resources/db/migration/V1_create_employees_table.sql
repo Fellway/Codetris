1eeecfcd28a696ae1ae88f5c1379a6294d701d9a
Kacper Rolewski <kacper.r17@gmail.com>
2021-03-12 15:26:03 +0100

    [COT-9] Removed unnecessary comments from SQL script that adds employees table

--- db-migration/src/main/resources/db/migration/V1_create_employees_table.sql
+++ db-migration/src/main/resources/db/migration/V1_create_employees_table.sql
@@ -1,16 +1,3 @@
--- Database: codestris
-
--- DROP DATABASE codestris;
-
-CREATE DATABASE codestris
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Polish_Poland.1250'
-    LC_CTYPE = 'Polish_Poland.1250'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-	
 CREATE TABLE IF NOT EXISTS employees (
 	id INT GENERATED ALWAYS AS IDENTITY,
 	first_name varchar(50) NOT NULL,
