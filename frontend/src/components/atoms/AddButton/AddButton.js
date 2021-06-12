5df600d64d2a0facce92ee2b3b9c11ef88687572
Fellway <mateusz.skrzypczyk1@hotmail.com>
2021-06-12 10:48:28 +0200

    Added addButton

---
+++ frontend/src/components/atoms/AddButton/AddButton.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import PlusIcon from 'assets/add.svg';
+import { theme } from '../../../theme/MainTheme';
+
+const AddButton = styled.div`
+  height: 80px;
+  width: 80px;
+  background-image: url(${PlusIcon});
+  background-repeat: no-repeat;
+  background-size: 50%;
+  background-color: ${theme.colors.primary};
+  background-position: center center;
+  border-radius: 50%;
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
+  transition: 0.5s;
+
+  :hover {
+    cursor: pointer;
+    box-shadow: 0px 0px 10px -1px ${theme.colors.primary};
+  }
+`;
+
+export default AddButton;
---
+++ frontend/src/components/atoms/AddButton/AddButton.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AddButtonComponent from './AddButton';
+
+export default {
+  title: 'Components/atoms/AddButton',
+  component: AddButtonComponent,
+};
+
+export const AddButton = () => <AddButtonComponent />;
